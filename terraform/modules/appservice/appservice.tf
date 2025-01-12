00072541a6b4c4c66b1091f7f0ae3c5be32dc7da
lechau <lehuyenchau12@gmail.com>
2025-01-12 23:17:45 +0700

    ..

--- terraform/modules/appservice/appservice.tf
+++ terraform/modules/appservice/appservice.tf
@@ -3,10 +3,8 @@ resource "azurerm_service_plan" "test" {
   location            = "${var.location}"
   resource_group_name = "${var.resource_group}"
   os_type             = "Linux"
-  sku {
-    tier = "Free"
-    size = "F1"
-  }
+  sku_name            = "F1"
+  sku_tier            = "Free"
 }
 
 resource "azurerm_linux_web_app" "test" {
