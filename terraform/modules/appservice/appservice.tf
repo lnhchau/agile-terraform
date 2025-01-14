fe34628f432c1f2d843a4aae50fc9c7d3635b0b7
lechau <lehuyenchau12@gmail.com>
2025-01-14 21:26:53 +0700

    ..

--- terraform/modules/appservice/appservice.tf
+++ terraform/modules/appservice/appservice.tf
@@ -2,11 +2,11 @@ resource "azurerm_service_plan" "test" {
   name                = "${var.application_type}-${var.resource_type}"
   location            = "${var.location}"
   resource_group_name = "${var.resource_group}"
-  os_type             = "Windows"
-  sku_name            = "B1"
+  os_type             = "Linux"
+  sku_name            = "S1"
 }
 
-resource "azurerm_windows_web_app" "test" {
+resource "azurerm_linux_web_app" "test" {
   depends_on          = [azurerm_service_plan.test] # Explicit dependency
   name                = "myapp-AppService-chaulnh-prj3"
   location            = var.location
@@ -17,10 +17,10 @@ resource "azurerm_windows_web_app" "test" {
     "WEBSITE_RUN_FROM_PACKAGE" = 0
   }
   site_config {
-    always_on = false
-    application_stack {
-      current_stack  = "dotnet"
-      dotnet_version = "v4.0"
-    }
+    # always_on = false
+    # application_stack {
+    #   current_stack  = "dotnet"
+    #   dotnet_version = "v4.0"
+    # }
   }
 }
