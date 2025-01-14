98a5b05ac4a3ea1a9584e647f37a7a43646ab18d
lechau <lehuyenchau12@gmail.com>
2025-01-14 20:47:12 +0700

    ..

--- terraform/modules/appservice/appservice.tf
+++ terraform/modules/appservice/appservice.tf
@@ -1,20 +1,26 @@
-resource "azurerm_app_service_plan" "test" {
+resource "azurerm_service_plan" "test" {
   name                = "${var.application_type}-${var.resource_type}"
   location            = "${var.location}"
   resource_group_name = "${var.resource_group}"
-  sku {
-    tier = "Free"
-    size = "F1"
-  }
+  os_type             = "Windows"
+  sku_name            = "B1"
 }
 
-resource "azurerm_app_service" "test" {
-  name                = "${var.application_type}-${var.resource_type}"
-  location            = "${var.location}"
-  resource_group_name = "${var.resource_group}"
-  app_service_plan_id      = azurerm_app_service_plan.test.id
+resource "azurerm_windows_web_app" "test" {
+  depends_on          = [azurerm_service_plan.test] # Explicit dependency
+  name                = "myapp-AppService-chaulnh-prj3"
+  location            = var.location
+  resource_group_name = var.resource_group
+  service_plan_id     = azurerm_service_plan.test.id
 
   app_settings = {
     "WEBSITE_RUN_FROM_PACKAGE" = 0
+  }
+  site_config {
+    always_on = false
+    application_stack {
+      current_stack  = "dotnet"
+      dotnet_version = "v4.0"
+    }
   }
 }
