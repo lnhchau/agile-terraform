5ed162ec12f3afffc58bdc3b94496675ec96944b
lechau <lehuyenchau12@gmail.com>
2025-01-15 13:12:11 +0700

    Update path for SSH key in VM

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -20,7 +20,9 @@ resource "azurerm_linux_virtual_machine" "test" {
   network_interface_ids = [azurerm_network_interface.test.id]
   admin_ssh_key {
     username   = "adminuser"
-    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDPyvVpGancHdw4ytXFoxkTA57nXPvWEXtSK2w00B89D3VLsPcKBLAx7EFeoGEgwPLvh46sFvZ6aQXoGCj2IiN4dn2K0FhlzFfjqWoJI27Es+untVW6lJSKcbm97OrpbH7MZX5NyvLoJ/vgjB9UJHO01fOGK2CO8ncksY/F/KTZUmFbsUp9SPtzFPSt0VG8jT2UFN20I78lYAwTwaNOQZnZDkgvG9vOyXOoFlOk7pMangyi33fEET+/s+NVvp5tMLAYNVAz8Jkrar7I0yHAyM3s8sGW8tT+PP1Jm+F7JuTN1OogdbdPLOSIF2vWMpZsGG7jdJ2382CC2EvdKZfpRDtn1NmLU9qwnnSRVlpAh6T1fV8qvktG9cajOSIvgm4UQAyAxREo8e232e7d0gh6zhHuYi+CXV/ePET14+Jpf/9xmJlsqZnBexfAtdT7/idZPK6ZxW7s2y3JKoz1udMVs8kdtU28mNubABKQAky8BlL0l2dnrm84Ge4KwP1avG/AruM= odl_user@SandboxHost-638725114068008484"
+    public_key = <<EOF
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDPyvVpGancHdw4ytXFoxkTA57nXPvWEXtSK2w00B89D3VLsPcKBLAx7EFeoGEgwPLvh46sFvZ6aQXoGCj2IiN4dn2K0FhlzFfjqWoJI27Es+untVW6lJSKcbm97OrpbH7MZX5NyvLoJ/vgjB9UJHO01fOGK2CO8ncksY/F/KTZUmFbsUp9SPtzFPSt0VG8jT2UFN20I78lYAwTwaNOQZnZDkgvG9vOyXOoFlOk7pMangyi33fEET+/s+NVvp5tMLAYNVAz8Jkrar7I0yHAyM3s8sGW8tT+PP1Jm+F7JuTN1OogdbdPLOSIF2vWMpZsGG7jdJ2382CC2EvdKZfpRDtn1NmLU9qwnnSRVlpAh6T1fV8qvktG9cajOSIvgm4UQAyAxREo8e232e7d0gh6zhHuYi+CXV/ePET14+Jpf/9xmJlsqZnBexfAtdT7/idZPK6ZxW7s2y3JKoz1udMVs8kdtU28mNubABKQAky8BlL0l2dnrm84Ge4KwP1avG/AruM= odl_user@SandboxHost-638725114068008484
+EOF
   }
   os_disk {
     caching           = "ReadWrite"
